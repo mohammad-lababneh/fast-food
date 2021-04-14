c5beca6c56e70c5f729c148059b6b1f5968563e1
mohammadlababne <>
2021-04-14 12:24:41 +0000

    food

---
+++ js/food.js
@@ -0,0 +1,29 @@
+
+ alert('Hello');
+ 
+var name = prompt('please enter your name');
+var name;
+alert (' welcome '+ name);
+
+  var userAge = prompt('please enter your age?');
+console.log('your age ' + userAge);
+
+var foodtype = prompt('do you like healthy food ? YES or NO')
+if (foodtype == 'YES'){ 
+  var image = '<img src="https://ventsmagazine.com/wp-content/uploads/2021/04/iStock-1131794876.t5d482e40.m800.xtDADj9SvTVFjzuNeGuNUUGY4tm5d6UGU5tkKM0s3iPk-620x342-1.jpg" alt= "healthy food"/>';
+  
+   document.write(image) ;
+
+
+
+
+}else if(foodtype == 'NO') {
+ var image2 = '<img src="https://scx1.b-cdn.net/csz/news/800a/2016/howcuttingdo.jpg" alt= "normal food"/>';
+document.write(image2) ;
+}
+ 
+
+
+
+
+
