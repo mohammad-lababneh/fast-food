44140c1043c65a78fbd8991757324dfc3b4ccfd8
mohammadlababne <>
2021-04-16 11:02:05 +0000

    food

--- js/food.js
+++ js/food.js
@@ -9,6 +9,10 @@ alert (' welcome '+ name);
 console.log('your age ' + userAge);
 
 var foodtype = prompt('do you like healthy food ? YES or NO')
+while (foodtype !== 'YES' && foodtype !== 'NO'){
+
+  var foodtype = prompt('do you like healthy food ? YES or NO')
+}
 if (foodtype == 'YES'){ 
   var image = '<img src="https://ventsmagazine.com/wp-content/uploads/2021/04/iStock-1131794876.t5d482e40.m800.xtDADj9SvTVFjzuNeGuNUUGY4tm5d6UGU5tkKM0s3iPk-620x342-1.jpg" alt= "healthy food"/>';
   
