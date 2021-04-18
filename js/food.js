4a25ad4a822632f2d2a558a8db04c8a708068f4d
mohammadlababne <>
2021-04-18 12:30:46 +0000

    food5

--- js/food.js
+++ js/food.js
@@ -1,19 +1,30 @@
 
- alert('Hello');
+ 
  
 var name = prompt('please enter your name');
 var name;
-alert (' welcome '+ name);
+customername(name)
 
-  var userAge = prompt('please enter your age?');
-console.log('your age ' + userAge);
+function customername(name)
+{
+alert('Hello '+ name);
 
-var foodtype = prompt('do you like healthy food ? YES or NO')
-if (foodtype == 'YES'){ 
+  var userAge = prompt(name + ' please enter your age?');
+var userAge;
+
+alert (name + ' your age is ' + userAge);
+}
+
+var foodtype = prompt('do you like healthy food ? yes or no')
+while (foodtype !== 'yes' && foodtype !== 'no'){
+
+  var foodtype = prompt('do you like healthy food ? yes or no')
+}
+if (foodtype == 'yes'){ 
   var image = '<img src="https://ventsmagazine.com/wp-content/uploads/2021/04/iStock-1131794876.t5d482e40.m800.xtDADj9SvTVFjzuNeGuNUUGY4tm5d6UGU5tkKM0s3iPk-620x342-1.jpg" alt= "healthy food"/>';
   
    document.write(image) ;
-}else if(foodtype == 'NO') {
+}else if(foodtype == 'no') {
  var image2 = '<img src="https://scx1.b-cdn.net/csz/news/800a/2016/howcuttingdo.jpg" alt= "normal food"/>';
 document.write(image2) ;
 }
@@ -23,12 +34,22 @@ while (type !== 'healthy' && type !== 'unhealthy'){
 
      var type = prompt ('enter the your favorite food please healthy food or unhealthy  ')
    }
-alert ('you like'+ type);
+alert ('you like '+ type +' food ');
 
  
 
+var ask;
+function askcustomer(add){
+  var ask = prompt('do you like to add ' + add + '?');
+  while (ask !== 'yes' && ask !== 'no'){
 
-
-
+  var ask = prompt('do you like to add ' + add + '?')
+}
+  return ask;
+}
+askcustomer('salt');
+askcustomer('ketchup');
+askcustomer('mayonnaise');
+askcustomer('cheese');
 
 
